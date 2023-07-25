9019571c3b5f55fcfab73b6b996bdcd1001b70a6
BiggyFlow <erickmpene@gmail.com>
2023-07-25 07:41:39 +0000

    commit

--- install-cluster/setup-worker-centos.sh
+++ install-cluster/setup-worker-centos.sh
@@ -41,7 +41,7 @@ sudo systemctl start containerd
 
 echo "####################################################"
 
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
