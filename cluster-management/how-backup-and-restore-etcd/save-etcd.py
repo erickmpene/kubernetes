86badb1585a99505e551e4f0156e2f6116044c56
BiggyFlow <erickmpene@gmail.com>
2023-08-31 21:00:12 +0000

    update

--- cluster-management/how-backup-and-restore-etcd/save-etcd.py
+++ cluster-management/how-backup-and-restore-etcd/save-etcd.py
@@ -7,7 +7,7 @@ import datetime
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
 
 # Backup file name with date and time prefix
-backup_file = f"/root/SSD/KUBERNETES/backup-etcd-dir/{timestamp}-M1_backup.db"
+backup_file = f"/data/backup-etcd-dir/{timestamp}-M1_backup.db"
 
 command = f'ETCDCTL_API=3 etcdctl --endpoints=https://127.0.0.1:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key snapshot save {backup_file}'
 
